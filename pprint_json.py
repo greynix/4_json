eabd2eefae994468a29806f2df58e6439b7f4b28
greynix <greynix@gmail.com>
2017-01-21 17:22:30 +0300

    changed pprint to json.dump and add argparse

--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,27 @@
 import json
-from pprint import pprint
-from sys import argv
+import argparse
 
 
 def load_data(filepath):
-    with open(filepath) as json_file:
+    with open(filepath,encoding='utf8') as json_file:
         return json.load(json_file)
 
 
 def pretty_print_json(data):
     """
-    We use pprint for 'pretty print' output
+    We use json.dumps for 'pretty print' output
+    data - input json
+    ensure_ascii - these characters will be output as-is and not be converted to unicode characters.
+    sort_keys - for sorting by key
+    indent - for indent each level
     """
-    return pprint(data)
+    return json.dumps(data,ensure_ascii=False,sort_keys=True,indent=4)
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data(argv[1]))
+    parser=argparse.ArgumentParser(description='''Display a json.''')
+    parser.add_argument('json_file', type=str, help='filepath')
+
+    user_input = parser.parse_args()
+
+    print(pretty_print_json(load_data(user_input.json_file)))
